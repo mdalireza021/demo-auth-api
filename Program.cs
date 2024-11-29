9ccbdf36114a9432567276262b7e10658c8a633f
md ali reza <muhammd.ali@brainstation-23.com>
2024-11-30 01:12:17 +0600

    add controller and custom validation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
+using demo_auth_api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+            .SelectMany(e => e.Value != null ? e.Value.Errors.Select(x => x.ErrorMessage) : []);
+
+        return new BadRequestObjectResult(new ApiResponse<List<string>>(
+            null, "", errors.ToList()));
+    };
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,4 +31,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
